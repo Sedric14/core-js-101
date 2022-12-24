5db5dc515838d97420da8881415bd40dbcd86ede
Sedric <sedricgreen6@gmail.com>
2022-12-24 23:17:28 +0200

    feat: 52 passing

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -19,6 +19,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
+// const _ = require('lodash');
+
 function findElement(arr, value) {
   // throw new Error('Not implemented');
   return arr.indexOf(value);
@@ -318,8 +320,11 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  // throw new Error('Not implemented');
+  arr.sort((a, b) => a - b);
+  arr.reverse();
+  return arr.slice(0, 3);
 }
 
 /**
@@ -335,8 +340,10 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  // throw new Error('Not implemented');
+  const arr2 = arr.filter((i) => typeof i === 'number' && i > 0);
+  return arr2.length;
 }
 
 /**
@@ -352,8 +359,20 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  // throw new Error('Not implemented');
+  const arr2 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const arr3 = [];
+  arr2.map((i) => {
+    arr.map((k) => {
+      if (k === i) {
+        arr3.push(k);
+      }
+      return k;
+    });
+    return i;
+  });
+  return arr3;
 }
 
 /**
@@ -368,8 +387,9 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  // throw new Error('Not implemented');
+  return arr.reduce((acc, val) => acc + val, 0);
 }
 
 /**
@@ -384,8 +404,14 @@ function getItemsSum(/* arr */) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  // throw new Error('Not implemented');
+  let count = 0;
+  arr.map((i) => {
+    if (!i) count += 1;
+    return i;
+  });
+  return count;
 }
 
 /**
@@ -402,8 +428,14 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  // throw new Error('Not implemented');
+  let count = 0;
+  arr.map((i) => {
+    if (i === item) count += 1;
+    return i;
+  });
+  return count;
 }
 
 /**
@@ -417,8 +449,9 @@ function findAllOccurrences(/* arr, item */) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  // throw new Error('Not implemented');
+  return arr.toString();
 }
 
 /**
@@ -447,8 +480,10 @@ function toStringList(/* arr */) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(r) {
+  // throw new Error('Not implemented');
+  const s = r.sort((a, b) => (a.country.localeCompare(b.country) || a.city.localeCompare(b.city)));
+  return s;
 }
 
 /**
